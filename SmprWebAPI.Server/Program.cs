afd793ddae760a81d90a1683fc63d0302af38c54
cnytkya <kayacuneyt2542@gmail.com>
2025-01-11 13:58:47 +0300

    added EmployeeRepository Scoped in Program.cs

--- SmprWebAPI.Server/Program.cs
+++ SmprWebAPI.Server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SmprWebAPI.Server.Data;
+using SmprWebAPI.Server.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<EmployeeRepository>();
 
 var app = builder.Build();
 
